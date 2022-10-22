1df6d090a0a18c6e70da8292d9485af27d57664c
Nabin Shrestha <shresthanabin1981@gmail.com>
2022-10-22 22:10:48 +0545

    feat: basic crud in nest

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDTO } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDTO) {
+    this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id, 10));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: Partial<UpdateUserDto>) {
+    return this.usersService.update(parseInt(id), body);
+  }
+}
---
+++ src/users/users.entity.ts
@@ -0,0 +1,37 @@
+import { IsEmail } from 'class-validator';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @IsEmail()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted user with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated user with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed user with id', this.id);
+  }
+}
